64192aa54f76b7e4ebfd55e29c3b34de2ccfa4a9
seungki.min <seungki.min@neowiz.com>
2026-02-25 09:47:26 +0900

    github actions 배포 테스트

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const repo = "daeyeon_homepage";
+
 const nextConfig: NextConfig = {
   /* config options here */
+    output: "export",
+    basePath: `/${repo}`,
+    assetPrefix: `/${repo}`,
 };
 
 export default nextConfig;
